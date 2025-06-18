3854adeb63608f0a3ae0b347bc1e9b5a9d6ed098
Thiago Marim <thiago.marim2005@gmail.com>
2025-06-18 16:43:39 -0300

    first commit

---
+++ src/main/java/me/dio/santander_bootcamp_2025_api/SantanderBootcamp2025ApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santander_bootcamp_2025_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcamp2025ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcamp2025ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santander_bootcamp_2025_api/SantanderBootcamp2025ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_bootcamp_2025_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcamp2025ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
