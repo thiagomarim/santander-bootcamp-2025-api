41bf689e863c0cd8d02af552a39c3edf322603a2
Thiago Marim <thiago.marim2005@gmail.com>
2025-06-18 16:46:57 -0300

    feat: add models

---
+++ src/main/java/me/dio/santander_bootcamp_2025_api/domain/model/Feature.java
@@ -0,0 +1,7 @@
+package me.dio.santander_bootcamp_2025_api.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+}
---
+++ src/main/java/me/dio/santander_bootcamp_2025_api/domain/model/News.java
@@ -0,0 +1,8 @@
+package me.dio.santander_bootcamp_2025_api.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
